79f9cc9e6949f6925ddc2f41d18cdc8624ca2706
Vinicius Coutinho <vac6548@gmail.com>
2026-07-19 13:29:04 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+        
+
+
+
+
+
+    }
+}
