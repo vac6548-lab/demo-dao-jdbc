a7587af703ae69a12e578a627c19fd09bc2d2e00
Vinicius Coutinho <vac6548@gmail.com>
2026-07-19 15:34:25 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
@@ -9,6 +12,7 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(),3000.00, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
 
 
